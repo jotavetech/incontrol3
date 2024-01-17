0815c33efa4781cd34974b4571b1f145df95c240
jotavetech <ojoaogugel@gmail.com>
2024-01-17 10:16:00 -0300

    feat: pwa start

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+import withPWA from "next-pwa";
+
+const nextConfig = {
+  ...withPWA({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  }),
+};
+
+module.exports = nextConfig;
