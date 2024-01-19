6ee096f2c8d1d58bbb095ea5ce2aeef276068ac4
jotavetech <ojoaogugel@gmail.com>
2024-01-19 09:59:18 -0300

    just testing

--- app/(main)/resume/[resumeId]/page.tsx
+++ app/(main)/resume/[resumeId]/page.tsx
@@ -1,5 +1,117 @@
+import { columns } from "@/components/table/columns";
+
+import { DataTable } from "@/components/table/data-table";
+
+import { Coins } from "lucide-react";
+
+const mockData = [
+  {
+    id: "1",
+    description: "Internet",
+    status: "Bill",
+    type: "Home",
+    amount: 130,
+  },
+  {
+    id: "1",
+    description: "Luz",
+    status: "Bill",
+    type: "Home",
+    amount: 220,
+  },
+  {
+    id: "1",
+    description: "Salario",
+    status: "Entrie",
+    type: "Work",
+    amount: 1500,
+  },
+  {
+    id: "1",
+    description: "Gasolina",
+    status: "bill",
+    type: "Car",
+    amount: 330,
+  },
+  {
+    id: "1",
+    description: "Banana",
+    status: "Bill",
+    type: "Shop",
+    amount: 12,
+  },
+  {
+    id: "1",
+    description: "Ração",
+    status: "bill",
+    type: "Animal",
+    amount: 130,
+  },
+
+  {
+    id: "1",
+    description: "Ração",
+    status: "bill",
+    type: "Animal",
+    amount: 130,
+  },
+  {
+    id: "1",
+    description: "Ração",
+    status: "bill",
+    type: "Animal",
+    amount: 130,
+  },
+  {
+    id: "1",
+    description: "Ração",
+    status: "bill",
+    type: "Animal",
+    amount: 130,
+  },
+  {
+    id: "1",
+    description: "Ração",
+    status: "bill",
+    type: "Animal",
+    amount: 130,
+  },
+  {
+    id: "1",
+    description: "Ração",
+    status: "bill",
+    type: "Animal",
+    amount: 130,
+  },
+  {
+    id: "1",
+    description: "Ração",
+    status: "bill",
+    type: "Animal",
+    amount: 130,
+  },
+  {
+    id: "1",
+    description: "Ração",
+    status: "bill",
+    type: "Animal",
+    amount: 130,
+  },
+];
+
 const ResumePage = () => {
-  return <div>resume page</div>;
+  return (
+    <div>
+      <div className="flex w-full justify-between items-center">
+        <h1 className="text-xl font-bold ml-4 my-4 md:ml-8 md:my-8 md:text-3xl flex gap-2 items-center">
+          <Coins /> Resume
+        </h1>
+      </div>
+      <div className="flex flex-col h-full w-full items-center md:items-start md:px-8">
+        <DataTable columns={columns} data={mockData} />
+      </div>
+    </div>
+  );
 };
 
 export default ResumePage;
---
+++ components/table/columns.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { MoreHorizontal, Trash, ArrowUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export type Item = {
+  id: string;
+  description: string;
+
+  type: string;
+  amount: number;
+};
+
+export const columns: ColumnDef<Item>[] = [
+  {
+    accessorKey: "description",
+    header: "Description",
+  },
+  {
+    accessorKey: "type",
+    header: "Type",
+  },
+  {
+    accessorKey: "amount",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Amount
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="font-semibold">Edit</DropdownMenuItem>
+            <DropdownMenuItem className="font-semibold text-red-400 flex items-center gap-2">
+              Delete <Trash />
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
