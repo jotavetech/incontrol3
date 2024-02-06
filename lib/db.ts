3abdd1c5a192fda90bb1745e55e4ccb40344db72
jotavetech <ojoaogugel@gmail.com>
2024-02-06 09:56:46 -0300

    try to fix role connections

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,25 @@
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+
+// export const db = globalThis.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
 import { PrismaClient } from "@prisma/client";
 
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+const db = globalThis.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+export { db };
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
