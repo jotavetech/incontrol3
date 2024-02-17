72f1dcf24775f180d926daa5a11bba1e75e0add7
jotavetech <ojoaogugel@gmail.com>
2024-02-17 16:56:07 -0300

    feat: add analytics page

---
+++ app/(main)/analytics/page.tsx
@@ -0,0 +1,85 @@
+import HomeMobileMenu from "@/components/home/home-mobile-menu";
+import RegistersResumeCard from "@/components/resume/registers-resume-card";
+import { Separator } from "@/components/ui/separator";
+import { currentUser } from "@/lib/current-user";
+import { db } from "@/lib/db";
+import { UserButton } from "@clerk/nextjs";
+import { BarChart3, BarChart4, Tags } from "lucide-react";
+import { redirect } from "next/navigation";
+
+const AnalyticsPage = async () => {
+  const user = await currentUser();
+
+  if (!user) return redirect("/sign-in");
+
+  const entriesRegisters = await db.entry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const expensesRegisters = await db.expense.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return (
+    <div>
+      <div className="flex w-full lg:max-w-[1600px]  justify-between items-center">
+        <h1 className="text-xl font-bold ml-4 my-4 md:ml-8 md:my-8 md:text-3xl flex items-center gap-2">
+          Analytics <BarChart4 />
+        </h1>
+        <div className="hidden md:flex mr-8 my-8">
+          <UserButton
+            afterSignOutUrl="/sign-in"
+            appearance={{
+              elements: { avatarBox: "w-12 h-12 " },
+            }}
+          />
+        </div>
+        <div className="flex md:hidden">
+          <HomeMobileMenu />
+        </div>
+      </div>
+      <div className="flex flex-col h-full w-full lg:max-w-[1600px] items-center md:items-start md:px-8 px-2">
+        <div className="w-full flex flex-col md:flex-row gap-5">
+          <Separator className="md:hidden" />
+          <p className="pl-4 font-semibold text-base flex gap-2 items-center md:hidden">
+            Here its your resume!
+          </p>
+          <div className="md:w-1/2">
+            <RegistersResumeCard type="entries" registers={entriesRegisters} />
+          </div>
+          <div className="md:w-1/2">
+            <RegistersResumeCard
+              type="expenses"
+              registers={expensesRegisters}
+            />
+          </div>
+        </div>
+        <div className="w-full lg:max-w-[1600px] mb-20">
+          <p className="pl-4 my-4 font-semibold text-base flex gap-2 items-center md:text-2xl md:my-8">
+            Most used categories <Tags className="w-4 h-4" />
+          </p>
+          <div className="w-full flex flex-col md:flex-row gap-5">
+            {/* <div className="md:w-1/2">
+              <HomeRegisterCard type="entries" registers={entries} />
+            </div>
+            <div className="md:w-1/2">
+              <HomeRegisterCard type="expenses" registers={expenses} />
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnalyticsPage;
---
+++ components/resume/registers-resume-card.tsx
@@ -0,0 +1,89 @@
+import { Minus, Plus, TrendingDown, TrendingUp } from "lucide-react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+
+import { cn, formatValue, getAtualDateRegisters, monthsMap } from "@/lib/utils";
+
+import { Entry, Expense } from "@prisma/client";
+
+type Register = Entry | Expense;
+
+interface RegistersResumeCardProps {
+  registers: Register[];
+  type: "entries" | "expenses";
+}
+
+const RegistersResumeCard = ({ registers, type }: RegistersResumeCardProps) => {
+  const allRegistersTotalAmount = () =>
+    registers.reduce((ac, curr) => ac + curr.amount, 0);
+
+  const thisMonthRegistersAmount = () => {
+    const atualDateRegisters = getAtualDateRegisters(registers);
+
+    return atualDateRegisters.reduce((ac, curr) => ac + curr.amount, 0);
+  };
+
+  return (
+    <Card className="md:h-[250px] rounded-2xl shadow-sm">
+      <CardHeader>
+        <CardTitle className="text-lg md:text-2xl flex gap-2 items-center capitalize justify-between">
+          <span className="capitalize">{type} Resume</span>
+          {type === "entries" ? (
+            <TrendingUp className="opacity-70" />
+          ) : (
+            <TrendingDown className="opacity-70" />
+          )}
+        </CardTitle>
+        <CardDescription className="text-xs md:text-base">
+          Total of all your {type}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p className="flex gap-2 items-center text-2xl">
+          <span
+            className={cn(
+              `text-red-400 flex items-center`,
+              type === "entries" && "text-green-400",
+              allRegistersTotalAmount() === 0 && "text-zinc-400"
+            )}
+          >
+            {type === "entries" ? (
+              <Plus className="w-4 h-4" />
+            ) : (
+              <Minus className="w-4 h-4" />
+            )}
+            {formatValue(allRegistersTotalAmount())}
+          </span>
+        </p>
+      </CardContent>
+      <CardFooter>
+        <p className="flex gap-2 items-center text-sm md:text-xl opacity-90">
+          Only {monthsMap[new Date().getMonth()]} Total:{" "}
+          <span
+            className={cn(
+              `text-red-400 flex items-center`,
+              type === "entries" && "text-green-400",
+              thisMonthRegistersAmount() === 0 && "text-zinc-400"
+            )}
+          >
+            {type === "entries" ? (
+              <Plus className="w-4 h-4" />
+            ) : (
+              <Minus className="w-4 h-4" />
+            )}
+            {formatValue(thisMonthRegistersAmount())}
+          </span>
+        </p>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default RegistersResumeCard;
